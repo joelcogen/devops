91c529d08fdbd4153c25c89a971b4bfaf9dd4d0d
Joel Cogen <joel@cogen.co>
2024-10-18 11:27:01 +0400

    copy the proper key for app, plus ~ support

--- devops.sh
+++ devops.sh
@@ -337,7 +337,7 @@ basic_setup() {
   echo -e "\033[0;34m>>> CREATE USER $USERNAME <<<\n\033[0m"
   ssh root@$NAME "adduser --gecos '' --disabled-password $USERNAME && \
     su $USERNAME -c 'mkdir ~/.ssh; chmod 700 ~/.ssh; touch ~/.ssh/authorized_keys; chmod 600 ~/.ssh/authorized_keys'"
-  scp ~/.ssh/id_rsa.pub root@$NAME:/home/$USERNAME/.ssh/authorized_keys
+  scp $(eval echo "$PRIVATE_KEY_PATH").pub root@$NAME:/home/$USERNAME/.ssh/authorized_keys
   ssh root@$NAME "chown $USERNAME:$USERNAME /home/$USERNAME/.ssh/authorized_keys && \
     su $USERNAME -c 'chmod 600 ~/.ssh/authorized_keys'"
   ssh $NAME "echo 'SSH connection successful'"
