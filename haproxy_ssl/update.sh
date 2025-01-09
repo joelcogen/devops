b0ec3852e5b9b9abec9f9fa7bc6157391b22ac86
Joel Cogen <joel@cogen.co>
2025-01-09 13:53:17 +0400

    fix ssl update script

--- haproxy_ssl/update.sh
+++ haproxy_ssl/update.sh
@@ -1,8 +1,10 @@
 #!/bin/bash -e
 
-certbot renew --quiet
+systemctl stop haproxy
+
+certbot renew --quiet --config-dir /certs
 
 cat /certs/live/haproxy/fullchain.pem /certs/live/haproxy/privkey.pem > /certs/haproxy.pem
 chmod 600 /certs/haproxy.pem
 
-systemctl reload haproxy
+systemctl start haproxy
